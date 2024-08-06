d6e94196ebddf6370606053099bbf9cc037fb701
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-07 02:50:53 +0530

    Solving Issues

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({ origin: `${process.env.FE_BASE_URL}`, credentials: true }))
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
 app.use("/app/v1/user", userRouter)
 app.use("/app/v1/message", messageRouter)
 app.listen(PORT, () => {
