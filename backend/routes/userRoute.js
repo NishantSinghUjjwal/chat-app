be6e798f6ccaf8e8cd79b6a552a265e04c222fc5
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-07 03:02:44 +0530

    Solving Issues

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { register, login, logout, getOtherUsers } = require('../controllers/userController');
 const authenticateUser = require('../middleware/authenticateUser');
+const allowCors = require('../middleware/allowCors');
 const userRouter = express.Router();
 userRouter.route("/register").post(register);
 userRouter.route("/login").post(login);
