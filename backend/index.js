fad5ee6c115873fe01d63314af04b47bc7cde3f8
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-07 02:02:29 +0530

    Solving CORS issues

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({ origin: '*', credentials: true }))
+app.use(cors({ origin: process.env.FE_BASE_URL, credentials: true }))
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/message", messageRouter)
 app.listen(PORT, () => {
