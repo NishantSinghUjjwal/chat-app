b1b653bc04991597ce5ac671d0df2928cbed7693
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-14 21:31:34 +0530

    solving cors

--- frontend/src/utils/apiClient.ts
+++ frontend/src/utils/apiClient.ts
@@ -2,11 +2,5 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 const apiClient = axios.create({
   baseURL: `${import.meta.env.VITE_BE_BASE_URL}/app/v1`, // Replace with your api base URL
-  headers: {
-    "Allow-Control-Access-Origin": import.meta.env.VITE_BE_BASE_URL,
-    "Access-Control-Allow-Credentials": true,
-    "Content-Type": "application/json", // Optional: Default headers
-   "Access-Control-Allow-Methods":["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
-  },
 });
 export default apiClient;
