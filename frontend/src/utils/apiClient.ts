4dbbfa0529df27e3d68f853ab1de2f322cd24a36
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-14 18:28:40 +0530

    Solved cors issues

--- frontend/src/utils/apiClient.ts
+++ frontend/src/utils/apiClient.ts
@@ -3,6 +3,7 @@ axios.defaults.withCredentials = true;
 const apiClient = axios.create({
   baseURL: `${import.meta.env.VITE_BE_BASE_URL}/app/v1`, // Replace with your api base URL
   headers: {
+    "Allow-Control-Access-Origin": import.meta.env.VITE_BE_BASE_URL,
     "Content-Type": "application/json", // Optional: Default headers
   },
 });
