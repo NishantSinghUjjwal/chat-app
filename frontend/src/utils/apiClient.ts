1676881076ccaa917a61f2402f8551e344d073c1
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-14 18:41:04 +0530

    solved issues

--- frontend/src/utils/apiClient.ts
+++ frontend/src/utils/apiClient.ts
@@ -4,6 +4,7 @@ const apiClient = axios.create({
   baseURL: `${import.meta.env.VITE_BE_BASE_URL}/app/v1`, // Replace with your api base URL
   headers: {
     "Allow-Control-Access-Origin": import.meta.env.VITE_BE_BASE_URL,
+    "Access-Control-Allow-Credentials": true,
     "Content-Type": "application/json", // Optional: Default headers
   },
 });
