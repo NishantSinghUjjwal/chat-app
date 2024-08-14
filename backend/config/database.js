ee5e974012222b7a195acf36ef400a3d4eb348eb
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-14 23:04:34 +0530

    Solving cors

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose")
 const connnectDB = async () => {
     try {
 
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("Database connected")
     } catch (err) {
         console.log(err)
