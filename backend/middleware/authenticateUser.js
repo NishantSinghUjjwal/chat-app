dac7c23e3755bf0566d3a4c4175ed46a8f49eb0d
NishantSinghUjjwal <nishantujjwal2000@gmail.com>
2024-08-19 18:14:51 +0530

    Solving issues

--- backend/middleware/authenticateUser.js
+++ backend/middleware/authenticateUser.js
@@ -3,10 +3,12 @@ const jwt = require("jsonwebtoken");
 const authenticateUser = async (req, res, next) => {
     try {
         const token = req.cookies.token;
+        console.log("token",token)
         if (!token) {
             return res.status(401).json({ message: "User not authenticated" })
         }
         const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY)
+        console.log('decode',decode)
         if (!decode) {
             return res.status(401).json({ message: "User not authenticated" })
         }
